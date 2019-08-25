509b225b3c2cebb4a582f9a7e43253a7bbe23e6b
MARKIDEV <47052158+MARKIDEV@users.noreply.github.com>
2019-08-25 16:12:20 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -5,27 +5,20 @@ import './App.css';
 import TimerDynamic from './TimerDynamic.js';
 
 export default class App extends Component {
-  state = {
-    hours: "00",
-    seconds: "00",
-    mins: "00",
-    time: "5000",
-    clicked:'false'
-  };
+  
   render() {
     
     return (
-      <div>
+      
         <div className="App">
-      <div className="timeinitial">
+
         
-        <h3>Input your desired time</h3>
-        <input type="number"   onChange={this.handleChange}/>
-         </div>
-      <TimerDynamic time={this.state.time}/>
+        <h3 className=" timeinitial">Input your desired time</h3>
+      
+      <TimerDynamic/>
       
     </div>
-      </div>
+    
     )
   }
 }
--- TimerDynamic.js
+++ TimerDynamic.js
@@ -1,72 +1,60 @@
 
 import React, { Component } from 'react'
-
+let
+time=9999900;
+let
+      hours= Math.floor(time / (60 * 60 * 1000));
+let
+      mins= Math.floor((time- hours * 60 * 60 * 1000) / (60 * 1000));
+let
+      seconds=Math.floor(
+        (time - hours * 60 * 60 * 1000 - mins * 60 * 1000) / 1000)
+      
+ 
+  
 
 export default class TimerDynamic extends Component {
-  state = {
-    hours: "00",
-    seconds: "00",
-    mins: "00",
-    time: "5000",
-    clicked:'false'
-  };
-  constructor (props){
-    super();
-    this.state=props.time;
-  }
-  //  handleChange = event =>{
-  //     this.setState ({input:event.target.value}) ;
-  //   }
-
-
-  // 
   
-  handlechangeTime = () => {
-    
-    this.setState({
      
-      hours: Math.floor(time / (60 * 60 * 1000)),
-      mins: Math.floor((time- hours * 60 * 60 * 1000) / (60 * 1000)),
-      seconds: Math.floor(
-        (time - hours * 60 * 60 * 1000 - mins * 60 * 1000) / 1000
-        )
-      })
- 
-  }
+  
   render() {
   
      return (
        
         <div className="timer">
-        
-        
-          <div className="Units">
-
           <div>
-            <p>{this.state.hours} </p>   
+           <p>  Initial Time: {time}</p>
+          <div >
+           
+<div className="Units">
+          <div >
+            <p >{hours} : </p>   
             <div>Hour</div>
           </div>
 
-          <div>:</div>
+          
 
-          <div>
-          <p>{this.state.mins} </p>   
+          <div    style={{marginLeft: "17px"}} >
+
+          <p >{mins} : </p>   
             <div>Minute</div>
           </div>
 
-          <div>:</div>
+         
 
-          <div>
-          <p>{this.state.seconds} </p>            
+          <div style={{marginLeft: "20px"}}>
+          <p  >{seconds} </p>            
             <div>Second</div>
           </div>
 
         </div>
         <div className="buttons">
-          <button onClick={() =>this.onChange()} className="btn">Start</button>
+          <button  className="btn">Start</button>
           <button className="btn">Reset</button>
         </div>
       </div>
+      </div>
+      </div>
     )
      }
-}
+    }
